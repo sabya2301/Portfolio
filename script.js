64dc71be4863bfa81225d1bc5ec9f7f5aaff9532
Sabyasachi Das <sabyasachidas@Sabyasachis-MacBook-Pro.local>
2026-05-25 19:03:42 +0530

    add blogs

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ if (footer) {
     firstLine.textContent = `Built by Sabyasachi Das. © ${year}`;
   }
 }
+
+const homeLink = document.getElementById("homeLink");
+if (homeLink) {
+  homeLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+}
+
+const contactModal = document.getElementById("contactModal");
+const viewContactBtn = document.getElementById("viewContactBtn");
+const closeContactBtn = document.getElementById("closeContactBtn");
+
+viewContactBtn.addEventListener("click", () => {
+  contactModal.classList.add("active");
+});
+
+closeContactBtn.addEventListener("click", () => {
+  contactModal.classList.remove("active");
+});
+
+contactModal.addEventListener("click", (e) => {
+  if (e.target === contactModal) {
+    contactModal.classList.remove("active");
+  }
+});
